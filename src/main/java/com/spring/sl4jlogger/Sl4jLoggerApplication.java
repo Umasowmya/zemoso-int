75b3f3d2fbcd81865039ab64f3c473535d7802f4
Umasowmya <uma.sowmya@zemosolabs.com>
2022-04-28 15:02:39 +0530

    logging functionality

---
+++ src/main/java/com/spring/sl4jlogger/Sl4jLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.sl4jlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sl4jLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sl4jLoggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/sl4jlogger/Sl4jLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sl4jlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sl4jLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
