1cd5ed893080b9bc58dcb1b5ab6b145e759cae35
Umasowmya <uma.sowmya@zemosolabs.com>
2022-04-28 16:37:37 +0530

    committed

--- src/main/java/com/spring/sl4jlogger/controller/LoggingController.java
+++ src/main/java/com/spring/sl4jlogger/controller/LoggingController.java
@@ -1,14 +1,13 @@
 package com.spring.sl4jlogger.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 public class LoggingController {
-
-    @RequestMapping("/logger")
+    @GetMapping("/logger")
     public String index() {
         log.trace("A TRACE Message");
         log.debug("A DEBUG Message");
